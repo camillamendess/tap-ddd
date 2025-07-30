a9627dcdfd2886f5ddd821a26191a32b6e2134d9
Camilla <milla_mendes83@hotmail.com>
2025-07-30 17:00:54 -0300

    Improve Operator Entity

--- src/domain/seller/entities/operator.entity.ts
+++ src/domain/seller/entities/operator.entity.ts
@@ -3,15 +3,38 @@ import { Id } from "../../common/value-objects/id.value-object";
 
 // Entidade Operator: representa um operador que pode ser um funcionário/membro da equipe de um vendedor.
 
-// TODO - Melhorar essa entidade
-
 export class Operator implements Entity {
   constructor(
     readonly id: Id,
     private _name: string
   ) { }
 
+  static create(input: CreateOperatorInput) {
+    const id = input.id ?? Id.generate();
+
+    const operator = new Operator(id, input.name);
+
+    operator.validate();
+
+    return operator;
+  }
+
+  private validate() {
+    if (!(this.id instanceof Id)) {
+      throw new Error("Invalid ID type");
+    }
+
+    if (!this._name || this._name.trim() === "") {
+      throw new Error("Name is required");
+    }
+  }
+
   get name(): string {
     return this._name;
   }
+}
+
+export interface CreateOperatorInput {
+  id: Id;
+  name: string;
 }
